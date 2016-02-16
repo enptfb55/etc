4b5e853aaa590d87c232a071167531c04de0892d
btmcg <btmcg@users.noreply.github.com>
2016-02-16 00:13:50 -0500

    install script and remove a.vim

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Filename:      ~/dotfiles/install.sh
+# Last modified: 2016.02.16
+
+ln -fs ${HOME}/dotfiles/bash_profile ${HOME}/.bash_profile
+ln -fs ${HOME}/dotfiles/bashrc ${HOME}/.bashrc
+ln -fs ${HOME}/dotfiles/gdbinit ${HOME}/.gdbinit
+ln -fs ${HOME}/dotfiles/gitconfig ${HOME}/.gitconfig
+ln -fs ${HOME}/dotfiles/htoprc ${HOME}/.htoprc
+ln -fs ${HOME}/dotfiles/i3config ${HOME}/.i3/config
+ln -fs ${HOME}/dotfiles/i3status.conf ${HOME}/.i3status.conf
+ln -fs ${HOME}/dotfiles/inputrc ${HOME}/.inputrc
+ln -fs ${HOME}/dotfiles/lftprc ${HOME}/.lftprc
+ln -fs ${HOME}/dotfiles/profile ${HOME}/.profile
+ln -fs ${HOME}/dotfiles/tmux.conf ${HOME}/.tmux.conf
+ln -Fs ${HOME}/dotfiles/vim ${HOME}/vim
+ln -fs ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
+ln -fs ${HOME}/dotfiles/xinitrc ${HOME}/.xinitrc
+ln -fs ${HOME}/dotfiles/Xmodmap ${HOME}/.Xmodmap
+ln -fs ${HOME}/dotfiles/Xresources ${HOME}/.Xresources
