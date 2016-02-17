5384d68f3f96cfcd9caf18973688545a4db27b3e
btmcg <btmcg@users.noreply.github.com>
2016-02-16 23:00:47 -0500

    Add dotfiles/tmux to install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Filename:      ~/dotfiles/install.sh
 # Last modified: 2016.02.16
 
-mkdir -p ${HOME}/.config/htop
-mkdir -p ${HOME}/.i3
+mkdir --parents ${HOME}/.config/htop
+mkdir --parents ${HOME}/.i3
 
-ln -sf ${HOME}/dotfiles/bash_profile ${HOME}/.bash_profile
-ln -sf ${HOME}/dotfiles/bashrc ${HOME}/.bashrc
-ln -sf ${HOME}/dotfiles/gdbinit ${HOME}/.gdbinit
-ln -sf ${HOME}/dotfiles/gitconfig ${HOME}/.gitconfig
-ln -sf ${HOME}/dotfiles/htoprc ${HOME}/.config/htop/htoprc
-ln -sf ${HOME}/dotfiles/i3config ${HOME}/.i3/config
-ln -sf ${HOME}/dotfiles/i3status.conf ${HOME}/.i3status.conf
-ln -sf ${HOME}/dotfiles/inputrc ${HOME}/.inputrc
-ln -sf ${HOME}/dotfiles/lftprc ${HOME}/.lftprc
-ln -sf ${HOME}/dotfiles/profile ${HOME}/.profile
-ln -sf ${HOME}/dotfiles/tmux.conf ${HOME}/.tmux.conf
-ln -sfn ${HOME}/dotfiles/vim ${HOME}/.vim
-ln -sf ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
-ln -sf ${HOME}/dotfiles/xinitrc ${HOME}/.xinitrc
-ln -sf ${HOME}/dotfiles/Xmodmap ${HOME}/.Xmodmap
-ln -sf ${HOME}/dotfiles/Xresources ${HOME}/.Xresources
+ln --force --symbolic ${HOME}/dotfiles/bash_profile ${HOME}/.bash_profile
+ln --force --symbolic ${HOME}/dotfiles/bashrc ${HOME}/.bashrc
+ln --force --symbolic ${HOME}/dotfiles/gdbinit ${HOME}/.gdbinit
+ln --force --symbolic ${HOME}/dotfiles/gitconfig ${HOME}/.gitconfig
+ln --force --symbolic ${HOME}/dotfiles/htoprc ${HOME}/.config/htop/htoprc
+ln --force --symbolic ${HOME}/dotfiles/i3config ${HOME}/.i3/config
+ln --force --symbolic ${HOME}/dotfiles/i3status.conf ${HOME}/.i3status.conf
+ln --force --symbolic ${HOME}/dotfiles/inputrc ${HOME}/.inputrc
+ln --force --symbolic ${HOME}/dotfiles/lftprc ${HOME}/.lftprc
+ln --force --symbolic ${HOME}/dotfiles/profile ${HOME}/.profile
+ln --force --symbolic ${HOME}/dotfiles/tmux.conf ${HOME}/.tmux.conf
+ln --force --no-dereference --symbolic ${HOME}/dotfiles/tmux ${HOME}/.tmux
+ln --force --no-dereference --symbolic ${HOME}/dotfiles/vim ${HOME}/.vim
+ln --force --symbolic ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
+ln --force --symbolic ${HOME}/dotfiles/xinitrc ${HOME}/.xinitrc
+ln --force --symbolic ${HOME}/dotfiles/Xmodmap ${HOME}/.Xmodmap
+ln --force --symbolic ${HOME}/dotfiles/Xresources ${HOME}/.Xresources
