243dff1ead6840ea3472148132444c9956422bdc
btmcg <btmcg@users.noreply.github.com>
2016-02-22 20:57:08 -0500

    Make bootstrap fully interactive

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,77 @@
 #!/usr/bin/env bash
 # Filename:      ~/dotfiles/bootstrap.sh
-# Last modified: 2016.02.21
+# Last modified: 2016.02.22
 
-mkdir --parents ${HOME}/.config/htop
-mkdir --mode=0700 --parents ${HOME}/.history
-mkdir --parents ${HOME}/.i3
-mkdir --parents ${HOME}/dotfiles/tmux/plugins
-mkdir --parents ${HOME}/dotfiles/vim/bundle
-mkdir --mode=0700 --parents ${HOME}/local/cache/mlocate
+function confirm()
+{
+    read -n 1 -r -p "${1:-Are you sure? [y/N]} " response
+    echo
+    case ${response} in
+        y)
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
 
-ln --force --symbolic ${HOME}/dotfiles/bash_profile ${HOME}/.bash_profile
-ln --force --symbolic ${HOME}/dotfiles/bashrc ${HOME}/.bashrc
-ln --force --symbolic ${HOME}/dotfiles/gdbinit ${HOME}/.gdbinit
-ln --force --symbolic ${HOME}/dotfiles/gitconfig ${HOME}/.gitconfig
-ln --force --symbolic ${HOME}/dotfiles/htoprc ${HOME}/.config/htop/htoprc
-ln --force --symbolic ${HOME}/dotfiles/hushlogin ${HOME}/.hushlogin
-ln --force --symbolic ${HOME}/dotfiles/i3config ${HOME}/.i3/config
-ln --force --symbolic ${HOME}/dotfiles/i3status.conf ${HOME}/.i3status.conf
-ln --force --symbolic ${HOME}/dotfiles/inputrc ${HOME}/.inputrc
-ln --force --symbolic ${HOME}/dotfiles/lftprc ${HOME}/.lftprc
-ln --force --symbolic ${HOME}/dotfiles/profile ${HOME}/.profile
-ln --force --symbolic ${HOME}/dotfiles/tmux.conf ${HOME}/.tmux.conf
-ln --force --no-dereference --symbolic ${HOME}/dotfiles/tmux ${HOME}/.tmux
-ln --force --symbolic ${HOME}/dotfiles/toprc ${HOME}/.toprc
-ln --force --no-dereference --symbolic ${HOME}/dotfiles/vim ${HOME}/.vim
-ln --force --symbolic ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
-ln --force --symbolic ${HOME}/dotfiles/xinitrc ${HOME}/.xinitrc
-ln --force --symbolic ${HOME}/dotfiles/Xmodmap ${HOME}/.Xmodmap
-ln --force --symbolic ${HOME}/dotfiles/Xresources ${HOME}/.Xresources
+MKDIR="mkdir --mode=0700 --parents"
+LN="ln --force --symbolic"
+LN_DIR="ln --force --no-dereference --symbolic"
+
+${MKDIR} ${HOME}/.history
+
+confirm "bash?" \
+    && ${LN} ${HOME}/dotfiles/bash_profile ${HOME}/.bash_profile \
+    && ${LN} ${HOME}/dotfiles/bashrc ${HOME}/.bashrc \
+    && ${LN} ${HOME}/dotfiles/profile ${HOME}/.profile
+
+confirm "gdb?" \
+    && ${LN} ${HOME}/dotfiles/gdbinit ${HOME}/.gdbinit
+
+confirm "git?" \
+    && ${LN} ${HOME}/dotfiles/gitconfig ${HOME}/.gitconfig
+
+confirm "htop?" \
+    && ${MKDIR} ${HOME}/.config/htop \
+    && ${LN} ${HOME}/dotfiles/htoprc ${HOME}/.config/htop/htoprc
+
+confirm "inputrc?" \
+    && ${LN} ${HOME}/dotfiles/inputrc ${HOME}/.inputrc
+
+confirm "hushlogin?" \
+    && ${LN} ${HOME}/dotfiles/hushlogin ${HOME}/.hushlogin
+
+confirm "i3?" \
+    && ${MKDIR} ${HOME}/.i3 \
+    && ${LN} ${HOME}/dotfiles/i3config ${HOME}/.i3/config \
+    && ${LN} ${HOME}/dotfiles/i3status.conf ${HOME}/.i3status.conf
+
+confirm "locate?" \
+    && ${MKDIR} ${HOME}/local/cache/mlocate
+
+confirm "lftp?" \
+    && ${LN} ${HOME}/dotfiles/lftprc ${HOME}/.lftprc
+
+confirm "tmux?" \
+    && ${MKDIR} ${HOME}/dotfiles/tmux/plugins \
+    && ${LN} ${HOME}/dotfiles/tmux.conf ${HOME}/.tmux.conf \
+    && ${LN_DIR} ${HOME}/dotfiles/tmux ${HOME}/.tmux
+
+confirm "top?" \
+    && ${LN} ${HOME}/dotfiles/toprc ${HOME}/.toprc
+
+confirm "vim?" \
+    && ${MKDIR} ${HOME}/dotfiles/vim/bundle \
+    && ${LN_DIR} ${HOME}/dotfiles/vim ${HOME}/.vim \
+    && ${LN} ${HOME}/dotfiles/vimrc ${HOME}/.vimrc
+
+confirm "xinit?" \
+    && ${LN} ${HOME}/dotfiles/xinitrc ${HOME}/.xinitrc
+
+confirm "xmodmap?" \
+    && ${LN} ${HOME}/dotfiles/Xmodmap ${HOME}/.Xmodmap
+
+confirm "xresources?" \
+    && ${LN} ${HOME}/dotfiles/Xresources ${HOME}/.Xresources
