3ff9a584a806f7cdafee189c4a51dabe2bec42b7
btmcg <btmcg@users.noreply.github.com>
2016-03-07 00:17:09 -0500

    Massive cleanup of bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,156 +1,185 @@
 #!/usr/bin/env bash
 # Filename:      etc/bootstrap.sh
 # Last modified: 2016.03.06
-
 # After cloning the etc.git repo into ${HOME}, this script will create the
 # necessary directory structure and symlinks.
 
-set -o errexit -o nounset -o pipefail
+set -o nounset -o pipefail
+
+# set up logging so that stdout and stderr go to a log file
+# note: to print only to console (original stdout), use echo "hello" 1>&3
+readonly LOG="${HOME}/var/log/bootstrap.log"
+exec 3>&1 1>>${LOG} 2>&1
+
+function log_debug() { echo "[$(date +'%Y.%m.%d %H:%M:%S%z')] [$$] [DEBUG] $@" >&2 ; }
+function log_info()  { echo "[$(date +'%Y.%m.%d %H:%M:%S%z')] [$$] [INFO ] $@" >&2 ; }
+function log_error() { echo "[$(date +'%Y.%m.%d %H:%M:%S%z')] [$$] [ERROR] $@" | tee /dev/fd/3 ; }
+function log_fatal() { echo "[$(date +'%Y.%m.%d %H:%M:%S%z')] [$$] [FATAL] $@" | tee /dev/fd/3 ; exit 1 ; }
 
 
 function main()
 {
-    local cmd_mkdir=$(get_cmd_mkdir $(uname -s))
-    local cmd_ln=$(get_cmd_ln $(uname -s))
+    log_info "$0"
 
-    [[ -z ${HOME} ]]      && echo error: HOME is not set && exit 1
-    [[ -z ${cmd_mkdir} ]] && echo error: Could not determine appropriate mkdir command && exit 1
-    [[ -z ${cmd_ln} ]]    && echo error: Could not determine appropriate ln command && exit 1
+    [[ -z ${HOME:-} ]] && log_error "error: HOME is not set" && exit 1
 
-    ${cmd_mkdir} ${HOME}/bin
-    ${cmd_mkdir} ${HOME}/src
-    ${cmd_mkdir} ${HOME}/var
-    ${cmd_mkdir} ${HOME}/var/log
-    ${cmd_mkdir} ${HOME}/var/xauthority
+    create_dir "${HOME}/bin"
+    create_dir "${HOME}/src"
+    create_dir "${HOME}/var"
+    create_dir "${HOME}/var/log"
+    create_dir "${HOME}/var/xauthority"
 
-    confirm "bash?" \
-        && ${cmd_ln} ${HOME}/etc/bash/bash_profile ${HOME}/.bash_profile \
-        && ${cmd_ln} ${HOME}/etc/bash/bashrc ${HOME}/.bashrc \
-        && ${cmd_mkdir} ${HOME}/var/bash \
-        && ${cmd_mkdir} ${HOME}/var/less \
-        && ${cmd_mkdir} ${HOME}/var/python \
-        && echo " [installed]"
+    confirm "bash" \
+        && create_symlink "${HOME}/etc/bash/bash_profile" "${HOME}/.bash_profile" \
+        && create_symlink "${HOME}/etc/bash/bashrc" "${HOME}/.bashrc" \
+        && create_dir "${HOME}/var/bash" \
+        && create_dir "${HOME}/var/less" \
+        && create_dir "${HOME}/var/python" \
+        && echo " [installed]" >&3
 
-    confirm "git?" \
-        && ${cmd_mkdir} ${HOME}/.config/git \
-        && ${cmd_ln} ${HOME}/etc/git/config ${HOME}/.config/git/config \
-        && echo " [installed]"
+    confirm "git" \
+        && create_dir "${HOME}/.config/git" \
+        && create_symlink "${HOME}/etc/git/config" "${HOME}/.config/git/config" \
+        && echo " [installed]" >&3
 
-    confirm "htop?" \
-        && ${cmd_mkdir} ${HOME}/.config/htop \
-        && ${cmd_ln} ${HOME}/etc/htop/htoprc ${HOME}/.config/htop/config \
-        && echo " [installed]"
+    confirm "htop" \
+        && create_dir "${HOME}/.config/htop" \
+        && create_symlink "${HOME}/etc/htop/htoprc" "${HOME}/.config/htop/config" \
+        && echo " [installed]" >&3
 
-    confirm "hushlogin?" \
-        && touch ${HOME}/.hushlogin \
-        && echo " [installed]"
+    confirm "hushlogin" \
+        && touch "${HOME}/.hushlogin" \
+        && echo " [installed]" >&3
 
-    confirm "i3?" \
-        && ${cmd_mkdir} ${HOME}/var/log \
-        && ${cmd_mkdir} ${HOME}/.config/i3 \
-        && ${cmd_mkdir} ${HOME}/.config/i3status \
-        && ${cmd_ln} ${HOME}/etc/i3/config ${HOME}/.config/i3/config \
-        && ${cmd_ln} ${HOME}/etc/i3status/config ${HOME}/.config/i3status/config \
-        && echo " [installed]"
+    confirm "i3" \
+        && create_dir "${HOME}/var/log" \
+        && create_dir "${HOME}/.config/i3" \
+        && create_dir "${HOME}/.config/i3status" \
+        && create_symlink "${HOME}/etc/i3/config" "${HOME}/.config/i3/config" \
+        && create_symlink "${HOME}/etc/i3status/config" "${HOME}/.config/i3status/config" \
+        && echo " [installed]" >&3
 
-    confirm "locate?" \
-        && ${cmd_mkdir} ${HOME}/var/cache/mlocate \
-        && echo " [installed]"
+    confirm "locate" \
+        && create_dir "${HOME}/var/cache/mlocate" \
+        && echo " [installed]" >&3
 
-    confirm "lftp?" \
-        && ${cmd_mkdir} ${HOME}/.config/lftp \
-        && ${cmd_ln} ${HOME}/etc/lftp/rc ${HOME}/.config/lftp/rc \
-        && echo " [installed]"
+    confirm "lftp" \
+        && create_dir "${HOME}/.config/lftp" \
+        && create_symlink "${HOME}/etc/lftp/rc" "${HOME}/.config/lftp/rc" \
+        && echo " [installed]" >&3
 
-    confirm "top?" \
-        && ${cmd_ln} ${HOME}/etc/top/toprc ${HOME}/.toprc \
-        && echo " [installed]"
+    confirm "top" \
+        && create_symlink "${HOME}/etc/top/toprc" "${HOME}/.toprc" \
+        && echo " [installed]" >&3
 
-    confirm "tmux?" \
-        && ${cmd_mkdir} ${HOME}/etc/tmux/plugins \
+    confirm "tmux" \
+        && create_dir "${HOME}/etc/tmux/plugins" \
         && install_tmux_plugins \
-        && echo " [installed]"
+        && echo " [installed]" >&3
 
-    confirm "vim?" \
-        && ${cmd_ln} ${HOME}/etc/vim ${HOME}/.vim \
-        && ${cmd_mkdir} ${HOME}/.vim/bundle \
-        && ${cmd_mkdir} ${HOME}/var/vim \
+    confirm "vim" \
+        && create_symlink "${HOME}/etc/vim" "${HOME}/.vim" \
+        && create_dir "${HOME}/.vim/bundle" \
+        && create_dir "${HOME}/var/vim" \
         && install_vim_plugins \
-        && echo " [installed]"
+        && echo " [installed]" >&3
 
 } # main()
 
 
 function confirm()
 {
-    read -n 1 -r -p "${1:-Are you sure? [y/N]} " response
+    (( "$#" != 1 )) && log_fatal "{confirm} missing argument"
+
+    # prompt user for y/n
+    echo -n "$1? " >&3
+
+    # capture response
+    read -n 1 -r response
+
     case ${response} in
-    y) return 0 ;;
-    *)
-        if [[ ${response} ]]; then
-            echo
-        fi
-        return -1 ;;
+    y)                                       return 0 ;;
+    *) [[ ! -z ${response} ]] && echo >&3 && return 1 ;;
     esac
-
 }
 
 
-function get_cmd_mkdir()
+function create_symlink()
 {
-    (( "$#" != 1 )) && return 1
+    (( "$#" != 2 )) && log_fatal "{create_symlink} missing argument to function"
 
-    case $1 in
+    case $(uname -s) in
 
     Linux)
-        echo "mkdir --mode=0700 --parents"
-        ;;
-
-    Darwin)
-        # if mkdir supports the --version option, it's GNU mkdir
-        if mkdir --version &> /dev/null; then
-            echo "mkdir --mode=0700 --parents"
-        else
-            echo "mkdir -m 0700 -p"
-        fi
-        ;;
-
-    *)
-        return 1
-        ;;
-
-    esac
-
-    return 0
-}
-
-
-function get_cmd_ln()
-{
-    (( "$#" != 1 )) && return 1
-
-    case $1 in
-
-    Linux)
-        echo "ln --force --no-dereference --relative --symbolic"
+        cmd="ln --force --no-dereference --relative --symbolic"
         ;;
 
     Darwin)
         # if ln supports the --version option, it's GNU ln
         if ln --version &> /dev/null; then
-            echo "ln --force --no-dereference --relative --symbolic"
+            cmd="ln --force --no-dereference --relative --symbolic"
         else
-            echo "ln -fhs"
+            cmd="ln -fhs"
         fi
         ;;
 
     *)
-        return 1
+        log_fatal "{create_symlink} unknown os [$(uname -s)]"
         ;;
 
     esac
 
-    return 0
+    log_info "{create_symlink} calling [${cmd} "$@"]"
+
+    output=$(${cmd} "$@" 2>&1)
+    local readonly exit_status=$?
+
+    if [[ "${exit_status}" -ne 0 ]]; then
+        log_error "{create_symlink} exit_status [${exit_status}]"
+        log_fatal "{create_symlink} ${output}"
+    else
+        log_debug "{create_symlink} exit_status [${exit_status}]"
+    fi
+}
+
+
+function create_dir()
+{
+    (( "$#" != 1 )) && log_fatal "{create_dir} missing argument to function"
+
+    # if directory already exists, we have nothing to do
+    [[ -d $1 ]] && log_debug "{create_dir} dir [$1] already exists" && return 0
+
+    case $(uname -s) in
+    Linux)
+        cmd="mkdir --mode=0700"
+        ;;
+
+    Darwin)
+        # if mkdir supports the --version option, it's GNU mkdir
+        if mkdir --version &> /dev/null; then
+            cmd="mkdir --mode=0700"
+        else
+            cmd="mkdir -m 0700"
+        fi
+        ;;
+
+    *)
+        log_fatal "{create_dir} unknown os [$(uname -s)]"
+        ;;
+    esac
+
+    log_info "{create_dir} calling [${cmd} $1]"
+
+    output=$(${cmd} $1 2>&1)
+    local readonly exit_status=$?
+
+    if [[ "${exit_status}" -ne 0 ]]; then
+        log_error "{create_dir} exit_status [${exit_status}]"
+        log_fatal "{create_dir} ${output}"
+    else
+        log_debug "{create_dir} exit_status [${exit_status}]"
+    fi
 }
 
 
@@ -158,15 +187,15 @@ function install_vim_plugins()
 {
     git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim &> /dev/null
     vim -e +PluginInstall +qall &> /dev/null
-    return 0
 }
 
 
 function install_tmux_plugins()
 {
-    git clone https://github.com/tmux-plugins/tpm ${HOME}/etc/tmux/plugins/tpm &> /dev/null
-    ${HOME}/etc/tmux/plugins/tpm/bin/update_plugins all &> /dev/null
+    git clone --quiet https://github.com/tmux-plugins/tpm ${HOME}/etc/tmux/plugins/tpm &>> ${HOME}/var/log/bootstrap.log
+    ${HOME}/etc/tmux/plugins/tpm/bin/install_plugins all &>> ${HOME}/var/log/bootstrap.log
 }
 
 
-main
+# main entry point
+main "$@"
